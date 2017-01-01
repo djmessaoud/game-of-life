2e5e8850440bfa0ae5a4ffa4c7747d2765015679
djmessaoud <djardi.messaoud@gmail.com>
2017-01-01 03:53:27 +0300

    Update my.h

--- src/my.h
+++ src/my.h
@@ -1,26 +1,30 @@
 #pragma once
 
-#define FSIZE 50
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+#define FSIZE 60
 #define DEAD ' '
-#define ALIVE 'o'
-
+#define ALIVE '°'
+using namespace std;
 struct field { char f[FSIZE][FSIZE]; };
-
-/*Drawing functions*/
 void printField(field& f);
 void csField(field& f);
-void drectangle(int h, int w, int x, int y, field& f);
-void dline(field& f);
-void dtriangle(int x, int y, int w, field& f);
-void dcircle(int x, int y, int r, field& f);
-void fillCell(int y,int x,field& f);
-void createPlaner(int x, int y, field& f);
-void dp(int x, int y, field& f);
-
-/*Geme of life functions*/
-void play(field& f, int maxIterations = 100);
+void play(field& f,field& f2,int steps);
 int neighbours(int y,int x,field& f);
 bool rule1(int y,int x,field& f);
 bool rule2(int y,int x,field& f);
 bool rule3(int y,int x,field& f);
 bool rule4(int y,int x,field& f);
+void fillCell(int y,int x,field& f);
+void createPlaner(int x, int y, field& f);
+void createSmallExploder(int x,int y,field& f);
+void createExploder (int x,int y,field& f);
+void createTenCellRow (int x,int y,field& f);
+void createSpaceship (int x,int y,field& f);
+void createTumbler (int x,int y,field& f);
+void gShape1 (int x,int y,field& f);
+void gShape2 (int x,int y,field& f);
+void gShape3 (int x,int y,field& f);
+void gShape4 (int x,int y,field& f);
+void createGosperGliperGun (int x,int y,field& f);
